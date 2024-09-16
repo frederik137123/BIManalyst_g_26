dcf983f540750d302daf886d58fe03b0926b3bb9
OliverBIM <s203614@dtu.dk>
2024-09-16 15:25:59 +0200

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ifcopenshell
 from .rules import windowRule
 from .rules import doorRule
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open('C:/Users/friis/OneDrive - Danmarks Tekniske Universitet/DTU/41934 - Advanced BIM/IFC/CES_BLD_24_06_STR.ifc')
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
